a72a6964f82704ff457f90adbd191a0e6dd2c00e
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2025-09-08 21:38:14 +0530

    added frotned proper redirection url to backend

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-console.log("FRONTEND_URL", process.env.FRONTEND_URL);
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
